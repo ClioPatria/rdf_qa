1626166ae0eb1746f847b26215638b6640c6367f
Jacco van Ossenbruggen <Jacco.van.Ossenbruggen@cwi.nl>
2012-01-31 11:54:50 +0100

    Add QA to ClioPatria Repository menu

--- config-available/rdf_qa.pl
+++ config-available/rdf_qa.pl
@@ -3,4 +3,8 @@
 /** <module> Heuristics for spotting problems in RDF data
 */
 
+
+:- use_module(cliopatria(hooks)).
 :- use_module(applications(rdf_qa)).
+
+cliopatria:menu_item(600=repository/rdf_qa_index, 'RDF quality heuristics').
